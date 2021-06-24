e512905f036eca6a0be58943300c211ba41af673
Zeapherine <zeapherine@gmail.com>
2021-06-24 13:08:52 +0530

    sign in components, custom button components are added

---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,11 @@
+
+import SignIn from '../../components/sign-in/sign-in.component'
+import './signin-and-signup.styles.scss';
+
+const SignInAndSignUp = () => (
+	<div className='sign-in-and-sign-up'>
+        <SignIn/>
+    </div>
+);
+
+export default SignInAndSignUp;
