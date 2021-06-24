d764853b9911cc65250521fba3924a69264365f8
Zeapherine <zeapherine@gmail.com>
2021-06-24 17:52:41 +0530

    implemented firebase utils, included ability to store authenticated user in firestore database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/signin-and-signup/signin-and-signup.component';
 import Header from './components/header/header.component';
 
-import { auth } from './firebase/firebase.util';
+import { auth, createUserProfileDocument } from './firebase/firebase.util';
 
 class App extends React.Component {
 	constructor() {
@@ -21,8 +21,22 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-			this.setState({ currentUser: user });
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+
+				userRef.onSnapshot((snapShot) => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data(),
+						},
+					});
+
+					console.log(this.state);
+				});
+			}
+			this.setState({ currentUser: userAuth });
 		});
 	}
 
--- src/pages/signin-and-signup/signin-and-signup.component.jsx
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -1,11 +1,13 @@
+import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
-import SignIn from '../../components/sign-in/sign-in.component'
 import './signin-and-signup.styles.scss';
 
 const SignInAndSignUp = () => (
 	<div className='sign-in-and-sign-up'>
-        <SignIn/>
-    </div>
+		<SignIn />
+		<SignUp />
+	</div>
 );
 
 export default SignInAndSignUp;
