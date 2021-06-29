90bf85bd3124e50b815f3cdf7326636d4ce24f02
Zeapherine <zeapherine@gmail.com>
2021-06-29 19:47:55 +0530

    moved directory and shop data into won reducer, converted shop data from array to object, created respective selectors and updated coresponding components with new ux flow for directory/shop

---
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import './collection-overview.styles.scss';
+
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+const CollectionOverview = ({ collections }) => (
+	<div className='collection-overview'>
+		{collections.map(({ id, ...otherCollectionProps }) => (
+			<CollectionPreview key={id} {...otherCollectionProps} />
+		))}
+	</div>
+);
+
+const mapStateToProps = createStructuredSelector({
+	collections: selectCollectionsForPreview,
+});
+
+export default connect(mapStateToProps)(CollectionOverview);
